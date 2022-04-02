911a14c227031b13391f79722c1fd84aa750c0b2
hieuthahn <hieuthahn@gmail.com>
2022-04-02 23:09:27 +0700

    Initial commit

---
+++ server/routes/auth.js
@@ -0,0 +1,109 @@
+const express = require("express")
+const router = express.Router()
+const argon2 = require("argon2")
+const jwt = require("jsonwebtoken")
+
+const User = require("../models/User")
+
+// @route POST apt/auth/register
+// @desc Register user
+// @access Public
+router.post("/register", async (req, res) => {
+    const { username, password } = req.body
+
+    // Simple validation
+
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ succes: false, message: "Missing username or password" })
+    }
+
+    try {
+        //Check for existing user
+        const user = await User.findOne({ username })
+
+        if (user) {
+            return res
+                .status(400)
+                .json({ succes: false, message: "User already exists" })
+        }
+
+        // All good
+        const hashedPassword = await argon2.hash(password)
+        const newUser = new User({ username, password: hashedPassword })
+        await newUser.save()
+
+        //Return token
+        const accessToken = jwt.sign(
+            { userId: newUser._id },
+            process.env.ACCESS_TOKEN_SECRET
+        )
+
+        res.json({
+            success: true,
+            message: "User created successfully",
+            accessToken: accessToken,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+})
+
+// @route POST apt/auth/login
+// @desc Login user
+// @access Public
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ succes: false, message: "Missing username or password" })
+    }
+
+    try {
+        //Check for existing user
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect username or password",
+            })
+        }
+
+        //User found
+        const passwordValid = await argon2.verify(user.password, password)
+        if (!passwordValid) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect username or password",
+            })
+        }
+
+        //All good
+        //Return token
+        const accessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN_SECRET
+        )
+
+        res.json({
+            success: true,
+            message: "User logged in successfully",
+            accessToken: accessToken,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+})
+
+module.exports = router
