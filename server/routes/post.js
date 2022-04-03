54a85bf292ce13396bc41ba2a6d4d5ebe08ff00a
hieuthahn <hieuthahn@gmail.com>
2022-04-03 14:08:16 +0700

    Update api and verify token middleware

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,12 +1,32 @@
 const express = require("express")
 const router = express.Router()
+const verifyToken = require("../middleware/auth")
 
 const Post = require("../models/Post")
+const { route } = require("./auth")
+
+// @route GET apit/posts
+// @desc GET post
+// @access Private
+router.get("/", verifyToken, async (req, res) => {
+    try {
+        const posts = await Post.find({ user: req.userId }).populate("user", [
+            "username",
+        ])
+        res.json({ success: true, posts })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+})
 
 // @route POST apit/posts
 // @desc Create post
 // @access Private
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
     const { title, description, url, status } = req.body
 
     // Simple validation
@@ -22,12 +42,93 @@ router.post("/", async (req, res) => {
             description,
             url: url.startsWith("https://") ? url : `https://${url}`,
             status: status || "TO LEARN",
-            user: "6247ecdc0a0b4dd46bfda883",
+            user: req.userId,
         })
 
         await newPost.save()
 
         res.json({ success: true, message: "Happy learning!", post: newPost })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+})
+
+// @route PUT apit/posts
+// @desc Update post
+// @access Private
+router.put("/:id", verifyToken, async (req, res) => {
+    const { title, description, url, status } = req.body
+
+    // Simple validation
+    if (!title) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Title is required" })
+    }
+
+    try {
+        let updatedPost = {
+            title,
+            description: description || "",
+            url: (url.startsWith("https://") ? url : `https://${url}`) || "",
+            status: status || "TO LEARN",
+        }
+
+        const postUpdateCondition = { _id: req.params.id, user: req.userId }
+
+        updatePost = await Post.findOneAndUpdate(
+            postUpdateCondition,
+            updatedPost,
+            { new: true }
+        )
+
+        // User not authorized to update post
+        if (!updatedPost) {
+            return res.status(401).json({
+                success: false,
+                message: "Post not found or user not authorized",
+            })
+        } else {
+            return res.json({
+                success: true,
+                message: "Post was updated",
+                post: updatedPost,
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+})
+
+// @route DELETE apit/posts
+// @desc Delete post
+// @access Private
+router.delete("/:id", verifyToken, async (req, res) => {
+    try {
+        const postDeleteCondition = { _id: req.params.id, user: req.userId }
+        const deletedPost = await Post.findOneAndDelete(postDeleteCondition)
+
+        // User not authorized or post not found
+        if (!deletedPost) {
+            return res.status(401).json({
+                success: false,
+                message: "Post not found or user not authorized",
+            })
+        } else {
+            return res.json({
+                success: true,
+                message: "Post was deleted",
+                post: deletedPost,
+            })
+        }
     } catch (error) {
         console.log(error)
         res.status(500).json({
